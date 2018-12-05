44e04c3644deba31b530a3d3b926b6e31976cbdd
Rachel <rmwilliams1070@gmail.com>
2018-12-05 18:52:56 +0100

    Updated MainActivity, added CalendarActivity and corresponding xml

--- SharedCalendarApp/app/src/main/java/com/ait/calender/team/sharedcalendarapp/MainActivity.kt
+++ SharedCalendarApp/app/src/main/java/com/ait/calender/team/sharedcalendarapp/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.ait.calender.team.sharedcalendarapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnTempCalendarList.setOnClickListener {
+            startActivity(
+                    Intent(this@MainActivity,
+                            CalendarActivity::class.java)
+            )
+        }
     }
+
 }
