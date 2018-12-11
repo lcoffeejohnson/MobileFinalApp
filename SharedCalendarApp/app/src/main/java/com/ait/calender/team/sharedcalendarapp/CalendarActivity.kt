c747a2553a457c8f1c67ce4a512a9e7ce816ace9
Rachel <rmwilliams1070@gmail.com>
2018-12-11 23:21:46 +0100

    added week activity, removed link to main activity, changed ui of calendar activity

--- SharedCalendarApp/app/src/main/java/com/ait/calender/team/sharedcalendarapp/CalendarActivity.kt
+++ SharedCalendarApp/app/src/main/java/com/ait/calender/team/sharedcalendarapp/CalendarActivity.kt
@@ -1,40 +1,26 @@
 package com.ait.calender.team.sharedcalendarapp
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_calendar.*
 
 class CalendarActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.nav_month -> {
-
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.nav_week -> {
-
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.nav_day -> {
-
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
-
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
         cvMain.setDate(System.currentTimeMillis(),false,true)
 
         fabAddEvent.setOnClickListener {
             showAddEventDialog()
+        }
+
+        btnWeekMonth.setOnClickListener {
+            startActivity(
+                    Intent(this@CalendarActivity,
+                    WeekActivity::class.java))
         }
     }
 
